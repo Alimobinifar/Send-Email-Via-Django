3e0c9def0f3c7ed95a949dbd173dff1b7b34d0e5
Alimobinifar <88401300+Alimobinifar@users.noreply.github.com>
2022-03-06 11:36:18 +0330

    Update forms.py

--- contact/forms.py
+++ contact/forms.py
@@ -5,4 +5,4 @@ class EmailForm(forms.Form):
     email = forms.EmailField()
     subject = forms.CharField(max_length=500, widget=forms.Textarea())
     text = forms.CharField(max_length=250, widget=forms.Textarea())
-    files = forms.FileField()
+#     files = forms.FileField() #you can also attach a file 
